afe9e7ce2defd23f40b447f39985043260440bf0
grigrarhub <andranik37@gmail.com>
2020-01-21 12:44:44 +0300

    4 commit

--- src/main/java/Zoo.java
+++ src/main/java/Zoo.java
@@ -33,39 +33,3 @@ class Zoo {
 // Изменения в файле
 
 
-class Zoopark implements Zooimpl   {
-    private final ZooAddStrategy strategy;
-    private Set<Animal> park = new HashSet<>();
-
-    public Zoopark(ZooAddStrategy strategy) {
-
-        this.strategy = strategy;
-    }
-
-    @Override
-    public void addAnimal(Animal animal) {
-        if(strategy.shouldAddAnimalToPark(animal))
-            park.add(animal);
-    }
-
-    @Override
-    public String getAnimalsByType(Animal animal) {
-        return animal.getTypeAnimal();
-    }
-
-    @Override
-    public void printPark() {
-        for (Animal animal: park) {
-            System.out.println(animal.sayHallo());
-        }
-
-    }
-
-    @Override
-    public Set<Animal> getPark() {
-        return park;
-    }
-
-
-}
-
